179e849e1b9809dbe3dfff71b73a1f571c3413c9
Michael <ciaowwu@gmail.com>
2023-02-28 15:22:59 -0800

    donate page

---
+++ src/components/donate/ImageButton/FormInput.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+type FormInputProps = {
+    title: string;
+    type: string;
+    tailwindCSS: string;
+};
+
+const FormInput = ({
+    title,
+    type,
+    tailwindCSS,
+}: FormInputProps) => {
+    const [value, setValue] = useState({text: 'placeholder',});
+
+    const handleInputChange  = (event: { persist: () => void; target: { value: any; }; }) => {
+        setValue((value) => ({
+            text: event.target.value,
+        }));
+    };
+
+    return (
+        <div className="w-full flex flex-col">
+            <label className="text-lg font-medium">
+                {title}
+            </label>
+            <input type={type} value={value.text} onChange={handleInputChange} required className={tailwindCSS}></input>
+        </div>
+    );
+};
+
+export default FormInput;
