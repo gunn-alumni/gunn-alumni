f4e9de1e7ab7311229dea12f427826edc6e535aa
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-02-28 23:10:22 -0800

    make login page

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -8,22 +8,27 @@ const Navbar = () => {
     <div className="flex justify-center flex-wrap p-4 bg-primary md:px-64 text-white font-sans font-bold">
       <div className="md:flex">
         <Link className="flex items-center space-x-2 px-5" href="/">
-          <Image src={titanIcon} alt="icon" />
+          <Image src={titanIcon} alt="icon" width={30} height={30} />
           <div>Gunn High School | Alumni</div>
         </Link>
       </div>
 
       <div className="flex justify-center flex-1 items-center space-x-8 text-sm m-2">
-          
-          <Link className="hover:underline" href="/about">About</Link>
-          <Link className="hover:underline" href="/classmates">Classmates</Link>
-          <Link className="hover:underline" href="/travel">Travel</Link>
-          <Link className="hover:underline" href="/donate">Donate</Link>
-          <button classname="">Login</button>
-        </div>
-        <div className="flex items-center">
-          
+        <Link className="hover:underline" href="/about">
+          About
+        </Link>
+        <Link className="hover:underline" href="/classmates">
+          Classmates
+        </Link>
+        <Link className="hover:underline" href="/travel">
+          Travel
+        </Link>
+        <Link className="hover:underline" href="/donate">
+          Donate
+        </Link>
+        <button className="">Login</button>
       </div>
+      <div className="flex items-center"></div>
     </div>
   );
 };
