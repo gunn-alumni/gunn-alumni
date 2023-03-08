7ed2a17efc68c8a851bbd8fd51907e524f9de80a
Dylan Lu <dylanelu@gmail.com>
2023-03-08 09:59:01 -0800

    layout fix

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -4,12 +4,12 @@ import PasswordCheck from "./PasswordCheck";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen">
       <PasswordCheck />
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
