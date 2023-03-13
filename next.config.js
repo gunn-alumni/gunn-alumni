ec4ae342d36ef79755c133ed6cd3b4585d9f2a08
Dylan Lu <dylanelu@gmail.com>
2023-03-13 14:10:41 -0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+const isProd = process.env.NODE_ENV === "production";
+
+const withImages = require("next-images");
+
+/*
+ * Gets the BASE_PATH from the command used to start this app.
+ * If BASE_PATH is specified but it does not start with a "/"
+ * then add it.
+ */
+function getBasePath() {
+  var basePath = "";
+
+  if (isProd && process.env.BASE_PATH) {
+    if (process.env.BASE_PATH.startsWith("/")) {
+      basePath = process.env.BASE_PATH;
+    } else {
+      basePath = "/" + process.env.BASE_PATH;
+    }
+  }
+
+  console.log("getBasePath() : isProd = " + isProd);
+  console.log("getBasePath() : basePath = " + basePath);
+
+  return basePath;
 }
 
-module.exports = nextConfig
+module.exports = withCSS(
+  withImages({
+    assetPrefix: getBasePath(),
+
+    publicRuntimeConfig: {
+      basePath: getBasePath(),
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  })
+);
