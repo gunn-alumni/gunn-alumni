2bc70a6627c84bfa38d32d2547338a117e5cdab2
SamanthaLesner <sammy.lesner@gmail.com>
2023-03-20 20:11:14 -0700

    Make-shift Login-in/out on the navbar

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import titanIcon from "@/../public/images/titanIcon.png";
 import { GiHamburgerMenu } from "react-icons/gi";
+import dylan from "@/../public/images/dylan.png";
+
+
+const isloggedIn = false;
 
 const Navbar = () => {
   return (
@@ -12,6 +16,7 @@ const Navbar = () => {
           <div>Gunn High School | Alumni</div>
         </Link>
       </div>
+
 
       <div className="flex justify-center flex-wrap items-center text-sm space-x-3 px-3">
         <Link className="hover:underline" href="/team">
@@ -27,6 +32,39 @@ const Navbar = () => {
           Donate
         </Link>
       </div>
+
+      <div className="flex justify-end flex-wrap items-center text-sm space-x-3 px-2 w-60">
+
+        {isloggedIn?
+        <div className="flex flex-col items-center">
+          <Link className="peer flex flex-row items-center space-x-3 hover:underline" href="/classmates">
+            <p>FirstName LastName</p>
+            <Image src={dylan} className=" w-9 h-9 rounded-full" alt="icon"/>
+          </Link>
+          
+          <div className="hidden peer-hover:flex hover:flex flex-col bg-primary p-4 space-x-3 top-12 drop-shadow-lg absolute rounded">
+            <ul className="flex flex-col">
+              <il>
+                <Link className="hover:underline" href="/">Account</Link>
+              </il>
+              <il>
+                <Link className="hover:underline" href="/">Logout</Link>
+              </il>
+              
+            </ul>
+          </div>
+          
+          </div>
+        :
+        <div className="flex justify-center flex-wrap items-center space-x-3">
+          <Link className="hover:underline" href="/"> Sign up </Link>
+          <Link className="hover:underline" href="/login"> Login</Link>
+        </div>
+        }
+
+      </div>
+
+      
     </div>
   );
 };
