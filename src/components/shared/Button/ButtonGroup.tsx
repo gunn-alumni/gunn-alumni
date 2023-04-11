b858c964eb97323cca31e7cdcceee7d437615830
Michael <ciaowwu@gmail.com>
2023-04-11 15:54:36 -0700

    react-slider added, Slider component created

--- src/components/shared/Button/ButtonGroup.tsx
+++ src/components/shared/Button/ButtonGroup.tsx
@@ -9,9 +9,11 @@ const ButtonGroup = ({
     buttons,
     initialState = 0,
 }: ButtonGroupProps) => {
+    var defaultWidthPercent = Math.round((1/buttons.length) * 100);
     for (let i in buttons) { //set default props (might be a better way to do this)
         buttons[i].classNameActive ? null : buttons[i].classNameActive = "bg-primary text-white";
         buttons[i].classNameInactive ? null : buttons[i].classNameInactive = "bg-gray-300 text-gray-700";
+        buttons[i].className ? null : buttons[i].className = `max-w-[${defaultWidthPercent}%]`;
     }
     const [active, setActive] = useState(buttons[initialState].value);
     return (
@@ -21,7 +23,7 @@ const ButtonGroup = ({
                     key={i}
                     type="button"
                     onClick={() => setActive(button.value)}
-                    className={`flex grow rounded-full text-md font-bold justify-center ${
+                    className={`flex box-border grow rounded-full text-md font-bold justify-center ${
                         (button.value === active) ? button.classNameActive : button.classNameInactive} ${button.className}`}
                     >
                     {button.value}
