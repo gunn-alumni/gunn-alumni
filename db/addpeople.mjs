44f757608c00ebfbd2b7dd86260d900e3ab528c2
9t8 <63334933+9t8@users.noreply.github.com>
2023-04-18 17:01:52 -0700

    add people from json

--- db/addpeople.mjs
+++ db/addpeople.mjs
@@ -12,15 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import fs from 'fs'
 import connect, { sql } from '@databases/sqlite-sync'
 
-connect('store.db3').query(sql`
-  INSERT INTO people (name, grad_year) VALUES
-    ('Bobby Boomer', 2000),
-    ('David Li', 2024),
-    ('Veeee Eeeer', 2024),
-    ('Zobby Zoomer', 2040),
-    ('Gunn Alumni Dylan', 1984),
-    ('Shitty Staff', NULL),
-    ('Example User', 1999)
-`)
+const db = connect('store.db3')
+
+for (const person of JSON.parse(fs.readFileSync('./db/fakepeople.json'))) {
+  db.query(sql`
+    INSERT INTO people (name, grad_year) VALUES
+      (${person.name}, ${person.grad_year})
+  `)
+}
