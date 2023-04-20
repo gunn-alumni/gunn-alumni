0c2aa6e4dcc663cf6eaa496c405b1f7868fb8f05
Dylan Lu <dylanelu@gmail.com>
2023-04-20 16:38:52 -0700

    julia do stuff

--- src/pages/protected.tsx
+++ src/pages/protected.tsx
@@ -1,23 +1,42 @@
 import { type NextPage } from 'next'
 import Router from 'next/router'
 import { useEffect } from 'react'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
-const Protected: NextPage = (): JSX.Element => {
+const Protected: NextPage = ({user}: any): JSX.Element => {
   const session = useSession();
   const supabase = useSupabaseClient();
 
   return <div>
-     {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
-      ) : (
-        <p>Account page will go here.</p>
-      )}
+    {user.name}
   </div>
 }
 
+
+export const getServerSideProps = async (ctx: any) => {
+  // Create authenticated Supabase Client
+  const supabase = createServerSupabaseClient(ctx)
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session)
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+
+  return {
+    props: {
+      initialSession: session,
+      user: session.user,
+    },
+  }
+}
 
 
 export default Protected
