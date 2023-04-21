10273b5652bbd694c64e58ad57a7616047398dfe
Dylan Lu <dylanelu@gmail.com>
2023-04-21 13:16:42 -0700

    login works

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import dylan from '@/../public/images/dylan.png'
 import titanIcon from '@/../public/images/titanIcon.png'
-import { signOut, useSession } from 'next-auth/react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 const Navbar = (): JSX.Element => {
+  const session = useSession()
+  const supabase = useSupabaseClient()
 
   return (
     <div className="flex justify-around flex-wrap bg-primary text-white font-sans font-bold p-3 z-[1]">
@@ -30,11 +32,11 @@ const Navbar = (): JSX.Element => {
         </Link>
       </div>
 
-      {/* <div className="flex justify-end flex-wrap items-center text-sm space-x-3 px-2 w-60">
-        {(status === 'authenticated')
+      <div className="flex justify-end flex-wrap items-center text-sm space-x-3 px-2 w-60">
+        {(session !== null)
           ? (
           <div className="flex justify-center flex-row items-center space-x-3">
-            <Link className="hover:underline" href="/" onClick={() => { void signOut() }}>Logout</Link>
+            <Link className="hover:underline" href="/" onClick={() => { supabase.auth.signOut().catch(e => { console.log(e) }) }}>Logout</Link>
 
             <Link className="flex space-x-3 flex-row items-center hover:underline" href="/profile">
               <Image src={dylan} className=" w-9 h-9 rounded-full" alt="icon"/>
@@ -43,11 +45,11 @@ const Navbar = (): JSX.Element => {
             )
           : <div className="flex justify-center flex-wrap items-center space-x-3">
           <Link className="hover:underline" href="/signup">Sign up</Link>
-          <Link className="hover:underline" href="/auth/signin">Sign In</Link>
+          <Link className="hover:underline" href="/login">Login</Link>
         </div>
         }
 
-      </div> */}
+      </div>
 
     </div>
   )
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,16 +6,26 @@ import Router from 'next/router'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 const LoginPage = (): JSX.Element => {
-  const [email, setEmail] = useState<string | undefined>('')
+  const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState<string | undefined>('')
+  const [error, setError] = useState<string>('')
   const session = useSession()
   const supabase = useSupabaseClient()
 
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>): void => {
     e.preventDefault()
 
-
+    supabase.auth.signInWithPassword({
+      email,
+      password
+    }).then((res) => {
+      if (res.error !== null) {
+        setError(res.error.message)
+      } else {
+        Router.push('/').catch(err => { console.log(err) })
+      }
+    })
+      .catch((err) => { console.log(err) })
   }
   return (
     <section className="">
@@ -43,7 +53,7 @@ const LoginPage = (): JSX.Element => {
                   type="email"
                   name="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  placeholder="name@gmail.com"
+                  placeholder="example@gmail.com"
                   value={email}
                   onChange={(e) => { setEmail(e.target.value) }}
                 />
@@ -75,7 +85,7 @@ const LoginPage = (): JSX.Element => {
               <input
                 className="w-full text-white bg-primary hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 type="submit"
-                value="Sign In"
+                value="Login"
               />
               {(error !== '') && <div className={'text-red-500'}>Error: {error}</div>}
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
