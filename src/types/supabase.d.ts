05a44d8beb4541bbea0cddccf9d0f200e146a41c
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-05-13 10:57:58 -0700

    fix types

--- src/types/supabase.d.ts
+++ src/types/supabase.d.ts
@@ -1,20 +1,81 @@
-interface Profile {
-  index: number;
-  id: string;
-  created_at: string;
-  preferred_name: string;
-  pfp?: string;
-  current_location?: string;
-  phone_num?: number;
-  email_display?: string;
-  bio?: string;
-  social_media?: string;
-  verified: boolean;
-}
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[];
 
-interface People {
-  index: number;
-  id: string;
-  name: string;
-  grad_year: number;
+export interface Database {
+  public: {
+    Tables: {
+      people: {
+        Row: {
+          grad_year: string | null;
+          id: string | null;
+          index: number;
+          name: string | null;
+        };
+        Insert: {
+          grad_year?: string | null;
+          id?: string | null;
+          index?: never;
+          name?: string | null;
+        };
+        Update: {
+          grad_year?: string | null;
+          id?: string | null;
+          index?: never;
+          name?: string | null;
+        };
+      };
+      profiles: {
+        Row: {
+          bio: string | null;
+          created_at: string | null;
+          current_location: string | null;
+          id: string;
+          index: number;
+          pfp: string | null;
+          phone_num: number | null;
+          preferred_name: string | null;
+          social_media: Json | null;
+        };
+        Insert: {
+          bio?: string | null;
+          created_at?: string | null;
+          current_location?: string | null;
+          id: string;
+          index?: never;
+          pfp?: string | null;
+          phone_num?: number | null;
+          preferred_name?: string | null;
+          social_media?: Json | null;
+        };
+        Update: {
+          bio?: string | null;
+          created_at?: string | null;
+          current_location?: string | null;
+          id?: string;
+          index?: never;
+          pfp?: string | null;
+          phone_num?: number | null;
+          preferred_name?: string | null;
+          social_media?: Json | null;
+        };
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
 }
