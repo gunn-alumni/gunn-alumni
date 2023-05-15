70663ad032f1c1b9a6a3d918d2d33ecac6470b51
Julia Kang <julia7hk@gmail.com>
2023-05-14 19:03:42 -0700

    rename back to profile.tsx

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -12,29 +12,28 @@ import { BsYoutube, BsDiscord, BsLinkedin, BsSnapchat } from 'react-icons/bs';
 import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
 import { TiSocialTwitter } from 'react-icons/ti';
 
-
 // TODO: store this type somewhere else!
 type ProfileData = {
-  userId: string,
-  userPfp: string,
-  name: string,
-  bio: string,
+  userId: string;
+  userPfp: string;
+  name: string;
+  bio: string;
   contact: {
-    location: string,
-    phone: string,
-    email: string,
+    location: string;
+    phone: string;
+    email: string;
     socialMedia: {
-      facebook: string,
-      instagram: string,
-      linkedin: string,
-      twitter: string,
-      youtube: string,
-      discord: string,
-      snapchat: string,
-      tiktok: string
-    }
-  }
-}
+      facebook: string;
+      instagram: string;
+      linkedin: string;
+      twitter: string;
+      youtube: string;
+      discord: string;
+      snapchat: string;
+      tiktok: string;
+    };
+  };
+};
 
 const oldDummyProfileData: ProfileData = {
   userId: '0000',
@@ -119,7 +118,8 @@ export default function ProfilePage() {
           console.log('Fetched the Data: ', data);
           setProfileData(data);
         }
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.log('ERRORRRRRR: ', error);
         setProfileData(dummyProfileData);
       });
@@ -130,15 +130,19 @@ export default function ProfilePage() {
   const [profileName, setProfileName] = useState('');
   const [profileImage, setProfileImage] = useState('');
   const [profileBio, setProfileBio] = useState('');
-  const [socialMedias, setSocialMedias] = useState(dummyProfileData.contact.socialMedia);
-  const [contacts, setContacts] = useState<Omit<ProfileData['contact'], 'socialMedia'>>(dummyProfileData.contact); // TODO: hacky typing, but the awkward object structure somewhat forces my hand here
+  const [socialMedias, setSocialMedias] = useState(
+    dummyProfileData.contact.socialMedia
+  );
+  const [contacts, setContacts] = useState<
+    Omit<ProfileData['contact'], 'socialMedia'>
+  >(dummyProfileData.contact); // TODO: hacky typing, but the awkward object structure somewhat forces my hand here
 
   function setProfileData(userData: ProfileData) {
     setProfileName(userData.name);
     setProfileImage(userData.userPfp || '/images/userIconx96.png');
     setProfileBio(userData.bio);
 
-    const {socialMedia, ...contacts} = userData.contact;
+    const { socialMedia, ...contacts } = userData.contact;
     setSocialMedias(socialMedia);
     setContacts(contacts);
   }
@@ -199,9 +203,13 @@ export default function ProfilePage() {
             <div className="flex gap-4 mx-auto flex-wrap justify-center mb-4">
               {/* TODO: abstract into component? */}
               {Object.entries(socialMedias).map(([key, value]) => {
-                const MediaIcon = socialMediaIconsList[socialMediaNamesList.indexOf(key)];
+                const MediaIcon =
+                  socialMediaIconsList[socialMediaNamesList.indexOf(key)];
                 return (
-                  <button className="bg-primary p-1 font-semibold text-white rounded" key={key}>
+                  <button
+                    className="bg-primary p-1 font-semibold text-white rounded"
+                    key={key}
+                  >
                     <Link href={value} target="_blank">
                       <MediaIcon
                         id={key + '_icon'}
@@ -209,7 +217,7 @@ export default function ProfilePage() {
                       />
                     </Link>
                   </button>
-                )
+                );
               })}
             </div>
           ) : (
@@ -222,9 +230,9 @@ export default function ProfilePage() {
                   </div>
                   <div className="bg-black font-bold w-[10px] border-[black] border-y-[3px]"></div>
                   <input
-                      type="text"
-                      placeholder="Enter Link Here"
-                      className="w-full placeholder:text-stone-600 px-[5px] outline-0 border-[black] border-x-[none] border-y-[3px]"
+                    type="text"
+                    placeholder="Enter Link Here"
+                    className="w-full placeholder:text-stone-600 px-[5px] outline-0 border-[black] border-x-[none] border-y-[3px]"
                   />
                   <button className="bg-slate-400 font-bold w-fit p-[5px_10px] border-[black] border-x-[3px] border-y-[3px] rounded-tr-[50px] rounded-br-[50px]">
                     X
@@ -245,38 +253,38 @@ export default function ProfilePage() {
             About Me
           </div>
           <AutoResizingTextArea
-              id="ta_content"
-              rows={5}
-              disabled={lockState === 'locked'}
-              className="w-full px-3.5 py-2 resize-none bg-white border rounded-lg disabled:bg-gray-100 focus:outline-none focus-visible:ring-[3px]"
+            id="ta_content"
+            rows={5}
+            disabled={lockState === 'locked'}
+            className="w-full px-3.5 py-2 resize-none bg-white border rounded-lg disabled:bg-gray-100 focus:outline-none focus-visible:ring-[3px]"
           >
             {profileBio}
           </AutoResizingTextArea>
         </div>
 
         {Object.entries(contacts).map(([key, value]) => (
+          <div
+            title="contact_wrapper"
+            id={key.toLowerCase() + '_wrapper'}
+            className="mb-6"
+            key={key}
+          >
             <div
-              title="contact_wrapper"
-              id={key.toLowerCase() + '_wrapper'}
-              className="mb-6"
-              key={key}
+              title="contact_title"
+              id={key.toLowerCase() + '_title'}
+              className="font-bold mb-1"
             >
-              <div
-                title="contact_title"
-                id={key.toLowerCase() + '_title'}
-                className="font-bold mb-1"
-              >
-                {key.charAt(0).toUpperCase() + key.toLowerCase().slice(1)}
-              </div>
-              <AutoResizingTextArea
-                  id="ta-content"
-                  rows={1}
-                  disabled={lockState === 'locked'}
-                  className="w-full px-3.5 py-2 resize-none bg-white border rounded-lg disabled:bg-gray-100 focus:outline-none focus-visible:ring-[3px]"
-              >
-                {value}
-              </AutoResizingTextArea>
+              {key.charAt(0).toUpperCase() + key.toLowerCase().slice(1)}
             </div>
+            <AutoResizingTextArea
+              id="ta-content"
+              rows={1}
+              disabled={lockState === 'locked'}
+              className="w-full px-3.5 py-2 resize-none bg-white border rounded-lg disabled:bg-gray-100 focus:outline-none focus-visible:ring-[3px]"
+            >
+              {value}
+            </AutoResizingTextArea>
+          </div>
         ))}
       </div>
 
