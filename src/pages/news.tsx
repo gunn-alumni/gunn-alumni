57f4ca2347432fcad4552ab00876ae4f520c2897
Julia Kang <julia7hk@gmail.com>
2023-05-15 04:32:20 -0700

    add temporary homepage landing section and meta tags to each page

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,8 +1,19 @@
+import Head from 'next/head';
+
 const News = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      Work in progress...
-    </div>
+    <>
+      <Head>
+        <title>Gunn Alumni | News</title>
+        <meta
+          name="description"
+          content="Keep up with current events at Gunn"
+        />
+      </Head>
+      <div className="w-full h-full flex justify-center items-center">
+        Work in progress...
+      </div>
+    </>
   );
 };
 
