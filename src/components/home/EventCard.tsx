55f185f6fa8827b0083c3f6a5f02d94e883e4372
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-05-16 10:10:45 -0700

    verify stuff (#116)
    * verify stuff
    
    * update homepage
    
    * progress on verify
    
    * verify email works

--- src/components/home/EventCard.tsx
+++ src/components/home/EventCard.tsx
@@ -6,7 +6,7 @@ interface EventCardProps {
 
 const EventCard = ({ title, date, url }: EventCardProps) => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-start bg-gray-200 rounded-lg p-4 w-full ">
       <div>
         <div className="text-lg font-semibold text-center">
           {date.toLocaleString('default', { month: 'short' }).toUpperCase()}
@@ -14,7 +14,7 @@ const EventCard = ({ title, date, url }: EventCardProps) => {
         <div className="text-3xl font-bold text-center">15</div>
       </div>
       <div className="px-8">
-        <div className="text-4xl font-semibold">{title}</div>
+        <div className="text-3xl">{title}</div>
       </div>
     </div>
   );
--- src/pages/user/verify.tsx
+++ src/pages/user/verify.tsx
@@ -1,59 +1,85 @@
-import React, { useEffect, useState } from 'react';
+import React, { FormEventHandler, useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useSupabaseClient, useSession } from '@supabase/auth-helpers-react';
+import Container from '@/components/shared/Container';
 
 const Verify = (): JSX.Element => {
   const router = useRouter();
   const session = useSession();
   const supabase = useSupabaseClient();
 
-  const handleEmail = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleEmail: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
     if (session === null) return;
 
-    supabase
-      .from('profiles')
-      .select('id,name,email')
-      .eq('id', session.user.id)
-      .then(({ data }) => {
-        if (data === null) {
-          console.log('User data not found');
-          return;
-        }
-
-        fetch('/api/send-verify-email', {
-          method: 'POST',
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: data[0].email,
-            name: data[0].name,
-            id: data[0].id
-          })
-        }).then((res) => {
-          console.log('Response received');
-          console.log(res);
-        });
-      });
+    fetch('/api/send-verify-email', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        first_name: firstName,
+        last_name: lastName,
+        id: session.user.id
+      })
+    }).then((res) => {
+      console.log('Response received');
+      console.log(res);
+    });
   };
 
   return (
-    <section className="">
+    <Container className="flex items-center justify-center flex-col">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto">
         <div className="text-xl font-semibold">Gunn High School | Alumni</div>
       </div>
-      <div className="w-full bg-white rounded-lg shadow sm:max-w-md">
-        <button
-          onClick={handleEmail}
-          className="bg-red-300 px-4 py-2 rounded-lg"
-        >
-          Click me to send email
-        </button>
+      <div className="w-full bg-white rounded-lg shadow sm:max-w-md p-8">
+        <form onSubmit={handleEmail}>
+          <label
+            htmlFor="first"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            First Name
+          </label>
+          <input
+            type="text"
+            name="first"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+            placeholder="Dylan"
+            value={firstName}
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+          <label
+            htmlFor="first"
+            className="block mb-2 text-sm font-medium text-gray-900"
+          >
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="last"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+            placeholder="Lu"
+            value={lastName}
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
+          <button className="bg-red-300 px-4 py-2 rounded-lg mt-4">
+            Click me to send email
+          </button>
+        </form>
       </div>
-    </section>
+    </Container>
   );
 };
 
