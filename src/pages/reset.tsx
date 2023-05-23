c7da86e28766de954cc4f03073b6a1f777438120
Julia Kang <julia7hk@gmail.com>
2023-05-22 21:40:16 -0700

    wip

--- src/pages/reset.tsx
+++ src/pages/reset.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
-export default function ResetPassword() {
+export default function Reset() {
   const [email, setEmail] = useState<string>('');
   const supabase = useSupabaseClient();
+  const server = process.env.NEXT_PUBLIC_SERVER_NAME;
+  const reset_url = `${server}/update-password`;
+
+  const [title, setTitle] = useState('Reset Password');
 
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -11,34 +15,13 @@ export default function ResetPassword() {
 
     // send email
     const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: 'http://localhost:3000'
+      redirectTo: reset_url
     });
+    setTitle('email sent! Please check your inbox');
     console.log(data);
     console.log(error);
 
     console.log('1 done');
-
-    /**
-     * Step 2: Once the user is redirected back to your application,
-     * ask the user to reset their password.
-     */
-    useEffect(() => {
-      supabase.auth.onAuthStateChange(async (event, session) => {
-        if (event == 'PASSWORD_RECOVERY') {
-          const newPassword = prompt(
-            'What would you like your new password to be?'
-          );
-          if (newPassword) {
-            const { data, error } = await supabase.auth.updateUser({
-              password: newPassword
-            });
-          }
-
-          if (data) alert('Password updated successfully!');
-          if (error) alert('There was an error updating your password.');
-        }
-      });
-    }, []);
   };
 
   return (
@@ -47,8 +30,9 @@ export default function ResetPassword() {
         <div className="w-full bg-white rounded-lg shadow sm:max-w-md">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Reset Password
+              {title}
             </h1>
+            <div>{reset_url}</div>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <input
                 type="email"
@@ -63,7 +47,7 @@ export default function ResetPassword() {
               <input
                 className="w-full text-white bg-primary hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 type="submit"
-                value="Login"
+                value="Send Password Reset Email"
               />
             </form>
           </div>
--- src/pages/update-password.tsx
+++ src/pages/update-password.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 export default function NewPassword() {
-  const [password, setPassword] = useState(null);
+  const [password, setPassword] = useState('');
   const supabase = useSupabaseClient();
 
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
@@ -13,6 +13,29 @@ export default function NewPassword() {
       password: new_password
     });
   };
+
+  /**
+   * Step 2: Once the user is redirected back to your application,
+   * ask the user to reset their password.
+   */
+
+  // useEffect(() => {
+  //   supabase.auth.onAuthStateChange(async (event, session) => {
+  //     if (event == 'PASSWORD_RECOVERY') {
+  //       const newPassword = prompt(
+  //         'What would you like your new password to be?'
+  //       );
+  //       if (newPassword) {
+  //         const { data, error } = await supabase.auth.updateUser({
+  //           password: newPassword
+  //         });
+  //       }
+
+  //       if (data) alert('Password updated successfully!');
+  //       if (error) alert('There was an error updating your password.');
+  //     }
+  //   });
+  // }, []);
 
   return (
     <>
@@ -35,7 +58,7 @@ export default function NewPassword() {
                   name="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  value={password}
+                  // value={password}
                   onChange={(e) => {
                     setPassword(e.target.value);
                   }}
@@ -44,7 +67,7 @@ export default function NewPassword() {
               <input
                 className="w-full text-white bg-primary hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 type="submit"
-                value="Login"
+                value="Confirm"
               />
             </form>
           </div>
