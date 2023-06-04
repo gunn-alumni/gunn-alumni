23d663d51f230442f76ff99c8991ce450158630e
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-06-04 11:04:58 -0700

    is it pnpm >??SDFKJLS:Djf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
