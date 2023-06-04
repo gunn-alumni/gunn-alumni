b7415c6b5a237c817c1228d4bf4c47ce6fa70991
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-06-04 10:51:41 -0700

    im gonna die bro AHHAHAHHAHA

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export function handler(req: NextApiRequest, res: NextApiResponse) {
+type Data = {
+  name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   res.status(200).json({ name: 'John Doe' });
 }
