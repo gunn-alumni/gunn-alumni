0db4acf636ba1c8491403bb4b00e725756e3418a
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-06-04 09:14:30 -0700

    ahhh

---
+++ netlify/functions/hello.ts
@@ -0,0 +1,13 @@
+import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' })
+  };
+};
+
+export { handler };
