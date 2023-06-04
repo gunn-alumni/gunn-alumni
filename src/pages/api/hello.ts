c29dfcd4ab5c0daae119c08438e3691a26722e87
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-06-04 09:22:19 -0700

    ahhh

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: 'John Doe' });
 }
