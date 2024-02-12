95be904e806198994c212f4f8fdc0d01a2fd9cce
Julia Kang <julia7hk@gmail.com>
2024-02-12 12:04:13 -0800

    Small changes (#139)
    * Fixed verify issue.  (#137)
    
    * Uploading Avatars (#121)
    
    * implemented the ability to upload avatars to supabase
    
    * table is now updated when uploading a pfp
    
    * Classmates Preview With Registered Users First (#122)
    
    * lots of classmates stuff
    
    * progress on fetching classmates preview
    
    * fix build error?
    
    * ugh john stop breaking stuffgit add .
    
    * ahhh
    
    * hopefully fix?
    
    * classmates works
    
    * update user profile
    
    * added classes (by year) (#124)
    
    * added classes (by year)
    
    * why is it not building?
    
    * uhh
    
    * Update Navbar.tsx (#119)
    
    * Update Navbar.tsx
    
    changed the setting and wrapping so it make sense for user
    
    * Update Navbar.tsx
    
    text wrap work
    
    * Update Navbar.tsx
    
    to albert and veer's /profile
    
    * Update Navbar.tsx
    
    * Changes to profile social media links, in progress
    
    * Fixed + button
    
    * Fixed linking error and removed chaff
    
    * minor changes to profile
    
    * Fixed default social media links
    
    ---------
    
    Co-authored-by: Albert <albert.paloalto@gmail.com>
    
    * Albert  (#125)
    
    * Update Navbar.tsx
    
    changed the setting and wrapping so it make sense for user
    
    * Update Navbar.tsx
    
    text wrap work
    
    * Update Navbar.tsx
    
    to albert and veer's /profile
    
    * Update Navbar.tsx
    
    * Changes to profile social media links, in progress
    
    * Fixed + button
    
    * Fixed linking error and removed chaff
    
    * minor changes to profile
    
    * Fixed default social media links
    
    * hide edit profile button when session id != profile id
    
    * quick fix
    
    ---------
    
    Co-authored-by: 3047322836 <3047322836@qq.com>
    Co-authored-by: Dylan Lu <dylanelu@gmail.com>
    Co-authored-by: Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
    
    * fix for login (#127)
    
    * create password reset feature (#126)
    
    * verify hook, check if verified (#128)
    
    * remove non functioning features from profile
    
    * push warning for verify
    
    * verify fix, no email sending
    
    * fix?
    
    * remove babelrc
    
    * Ahhhhh why is netlify stupid
    
    * package.json
    
    * fixed finallly
    
    ---------
    
    Co-authored-by: Ethan Zhang <31393290+edzdez@users.noreply.github.com>
    Co-authored-by: SaraD <46728660+SaraD-666@users.noreply.github.com>
    Co-authored-by: Albert <albert.paloalto@gmail.com>
    Co-authored-by: dudua1 <62454511+dudua1@users.noreply.github.com>
    Co-authored-by: 3047322836 <3047322836@qq.com>
    Co-authored-by: Julia Kang <julia7hk@gmail.com>
    
    * update
    
    * Fix classmates alignment (#138)
    
    * move team page to the right on nav
    
    * add lily's news page
    
    * fix build errors
    
    ---------
    
    Co-authored-by: Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
    Co-authored-by: Ethan Zhang <31393290+edzdez@users.noreply.github.com>
    Co-authored-by: SaraD <46728660+SaraD-666@users.noreply.github.com>
    Co-authored-by: Albert <albert.paloalto@gmail.com>
    Co-authored-by: dudua1 <62454511+dudua1@users.noreply.github.com>
    Co-authored-by: 3047322836 <3047322836@qq.com>
    Co-authored-by: John Li (Tet) <tet@tet.moe>

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="z-10 w-full p-4 bg-gray-800 shadow md:flex md:items-center md:justify-between md:p-6">
       <span className="text-sm text-gray-400 sm:text-center">
-        © 2023 Gunn Capstone. All Rights Reserved.
+        © 2024 Gunn Capstone. All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-400 sm:mt-0">
         <li>
