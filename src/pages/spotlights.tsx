a840352cbf5bb18173e77e5088761e39902b8c94
Albert <albert.paloalto@gmail.com>
2024-03-09 22:40:08 -0800

    Small const change

--- src/pages/spotlights.tsx
+++ src/pages/spotlights.tsx
@@ -4,16 +4,24 @@ import StandardButton from '@/components/shared/Button/StandardButton';
 import NotableAlumCard from '@/components/spotlights/NotableAlumCard';
 import NotableAlumPreview from '@/components/spotlights/NotableAlumPreview';
 
-const dummyTags = ["Education", "Fish", "Agriculture", "Visual & Pref Arts", "Medicine"]
-let teehee = 0
-let dummyData : any[] = []
+const dummyTags = [
+  'Education',
+  'Fish',
+  'Agriculture',
+  'Visual & Pref Arts',
+  'Medicine'
+];
+let teehee = 0;
+const dummyData: any[] = [];
 for (let i = 0; i < 20; i++) {
   dummyData.push({
-    id: "asdf", class_title: 1809,
+    id: 'asdf',
+    class_title: 1809,
     tag: dummyTags[teehee],
-    story_content: "hello eliu fbwer u fhoiuw efhouse ffnoesi nhorfnh enfrhio friofro dfihod fufihu dfhyuu uuuuuu ujn nhn n nnnn kunv ",
-    first_name: "Poshua",
-    last_name: "Jaley"
+    story_content:
+      'hello eliu fbwer u fhoiuw efhouse ffnoesi nhorfnh enfrhio friofro dfihod fufihu dfhyuu uuuuuu ujn nhn n nnnn kunv ',
+    first_name: 'Poshua',
+    last_name: 'Jaley'
   });
   teehee++;
   if (teehee >= dummyTags.length) teehee = 0;
@@ -34,8 +42,8 @@ const Spotlights = () => {
           </h1>
           <p className="prose prose-xl mb-8 text-gray-500 px-0">
             The mission of the University of Gunn is to foster leaders and
-            citizens who challenge the present, enrich the future, and contribute
-            to a better world. Our alumni’s remarkable identities and
+            citizens who challenge the present, enrich the future, and
+            contribute to a better world. Our alumni’s remarkable identities and
             accomplishments are what make them Titans. Victors. The Leaders and
             Best.
           </p>
