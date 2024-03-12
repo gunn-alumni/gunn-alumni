5c973035b9c625a555418df0c9f4202de069b762
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2024-03-11 18:27:30 -0700

    Get site ready for Paley's presentation on Tuesday (#147)
    * Readding icons (#142)
    
    * Notable Alumni page text (in progress) (#144)
    
    * Update README.md
    
    * Notable alum cards (#145)
    
    * added sample spotlight (#146)
    
    * make profile card not stupid
    
    * hopefully fix build error
    
    * fix build errors
    
    ---------
    
    Co-authored-by: dudua1 <62454511+dudua1@users.noreply.github.com>
    Co-authored-by: Jesse <32250628+BlueberryTech123@users.noreply.github.com>

--- src/pages/spotlights.tsx
+++ src/pages/spotlights.tsx
@@ -1,17 +1,87 @@
 import Head from 'next/head';
+import Container from '@/components/shared/Container';
+import StandardButton from '@/components/shared/Button/StandardButton';
+import NotableAlumCard from '@/components/spotlights/NotableAlumCard';
+import NotableAlumPreview from '@/components/spotlights/NotableAlumPreview';
 
-const Spotlights = () => {
+// const dummyTags = [
+//   'Education',
+//   'Fish',
+//   'Agriculture',
+//   'Visual & Pref Arts',
+//   'Medicine'
+// ];
+// let teehee = 0;
+// const dummyData: any[] = [];
+// for (let i = 0; i < 1; i++) {
+//   dummyData.push({
+//     id: 'elizabeth',
+//     class_title: 1809,
+//     tag: dummyTags[teehee],
+//     story_content:
+//       'Elizabeth Chang-Davidson, a 2015 graduate, was deeply involved in Gunn High Schools community.',
+//     first_name: 'Elizabeth',
+//     last_name: 'Chang',
+//     profiles: {
+//       pfp: 'https://media.licdn.com/dms/image/D4E03AQF9g4FgyTvQFg/profile-displayphoto-shrink_800_800/0/1696019908234?e=2147483647&v=beta&t=4l3Lm_1_o2HazFWag1fFuu28ewDfOhJSxWKh6Xl9J5I'
+//     }
+//   });
+//   teehee++;
+//   if (teehee >= dummyTags.length) teehee = 0;
+// }
+
+const dummyData = [
+  {
+    id: 'elizabeth',
+    class_title: 1809,
+    tag: 'Education',
+    story_content:
+      'Elizabeth Chang-Davidson, a 2015 graduate, was deeply involved in Gunn High Schools community.',
+    first_name: 'Elizabeth',
+    last_name: 'Chang',
+    profiles: {
+      pfp: 'https://media.licdn.com/dms/image/D4E03AQF9g4FgyTvQFg/profile-displayphoto-shrink_800_800/0/1696019908234?e=2147483647&v=beta&t=4l3Lm_1_o2HazFWag1fFuu28ewDfOhJSxWKh6Xl9J5I'
+    }
+  }
+];
+
+export default function Spotlights() {
   return (
     <>
       <Head>
         <title>Gunn Alumni | Spotlights</title>
         <meta name="description" content="Notable Gunn Alumni" />
       </Head>
-      <div className="w-full h-full flex justify-center items-center">
-        Work in progress...
-      </div>
+      <Container>
+        <div className="my-20">
+          <h1 className="mb-8 text-4xl font-bold md:text-5xl">
+            {' '}
+            Notable Alumni
+          </h1>
+          <p className="prose prose-xl mb-8 text-gray-500 px-0">
+            The mission of the University of Gunn is to foster leaders and
+            citizens who challenge the present, enrich the future, and
+            contribute to a better world. Our alumnis remarkable identities and
+            accomplishments are what make them Titans. Victors. The Leaders and
+            Best.
+          </p>
+          <h3 className="text-xl text-black text-left">
+            {' '}
+            {/*<div className="m-2">
+              Know someone who belongs on this page?
+              <br />
+              Please consider nominating them!
+    </div>*/}
+            <div>
+              <StandardButton className="m-1" color={'bg-black'}>
+                Nominate an Alum
+              </StandardButton>
+            </div>
+          </h3>
+        </div>
+
+        <NotableAlumPreview peopleArr={dummyData}></NotableAlumPreview>
+      </Container>
     </>
   );
-};
-
-export default Spotlights;
+}
